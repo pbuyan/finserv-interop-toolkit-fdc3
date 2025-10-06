99f6c9c60a4aa6bca5dbf3d2c1046d9c7a53babb
p_buian <p.buyan@gmail.com>
2025-10-05 23:24:22 -0400

    refactor: change name property to readonly in InteropioAdapter class

--- adapters/interopio/src/index.ts
+++ adapters/interopio/src/index.ts
@@ -1,4 +1,3 @@
-
 export interface Adapter {
   name: 'interopio';
   start(): Promise<void>;
@@ -9,10 +8,20 @@ export interface Adapter {
 }
 
 export class InteropioAdapter implements Adapter {
-  name = 'interopio';
-  async start() { /* TODO */ }
-  async stop() { /* TODO */ }
-  async broadcast(_context: any, _channelId?: string) { /* TODO */ }
-  async raiseIntent(_intent: string, _context?: any) { return { handledBy: 'placeholder' }; }
-  async joinUserChannel(_channelId: string) { /* TODO */ }
+  readonly name = 'interopio';
+  async start() {
+    /* TODO */
+  }
+  async stop() {
+    /* TODO */
+  }
+  async broadcast(_context: any, _channelId?: string) {
+    /* TODO */
+  }
+  async raiseIntent(_intent: string, _context?: any) {
+    return { handledBy: 'placeholder' };
+  }
+  async joinUserChannel(_channelId: string) {
+    /* TODO */
+  }
 }
